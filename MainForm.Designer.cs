b1f1717f10b567188726140bb91dfde83631a180
201147428 <201147428@P148-05>
2017-11-16 15:58:20 -0500

    Mettre un button

--- MainForm.Designer.cs
+++ MainForm.Designer.cs
@@ -29,6 +29,7 @@
         private void InitializeComponent()
         {
             this.PN_Graphic = new GraphicManager.DoubleBufferPanel();
+            this.button1 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // PN_Graphic
@@ -41,11 +42,21 @@
             this.PN_Graphic.Size = new System.Drawing.Size(668, 481);
             this.PN_Graphic.TabIndex = 0;
             // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(30, 13);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
             // MainForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(688, 551);
+            this.Controls.Add(this.button1);
             this.Controls.Add(this.PN_Graphic);
             this.Name = "MainForm";
             this.Text = "Form1";
@@ -57,6 +68,7 @@
         #endregion
 
         private DoubleBufferPanel PN_Graphic;
+        private System.Windows.Forms.Button button1;
     }
 }
 
